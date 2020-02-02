cc21b3a689f824e1445df7d52869c7ed8ce546d1
inv0w <jonjoninfante@gmail.com>
2020-02-01 21:08:03 -0800

    Pattern in strings

--- Code/strings.py
+++ Code/strings.py
@@ -31,7 +31,7 @@ def find_all_indexes(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     indices = []
-    if len(pattern) < 1: #Edge case if pattern is an empty space
+    if len(pattern) < 1: #Edge case if pattern is None
         for i in range(len(text)):
             indices.append(i)
         return indices
